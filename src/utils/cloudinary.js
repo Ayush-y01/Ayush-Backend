3d3cf168e61aba6e5c7dd19f4d74d866b518458e
Ayush-y01 <Ayush-y01>
2025-06-25 22:05:28 +0530

    cloudinary config and upload

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,43 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { log } from 'console';
+import fs from "fs";
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET 
+    });
+
+
+    const uploadOnCloud = async (localFilePath) => {
+        try {
+            if (!localFilePath) return null
+            const response = await cloudinary.uploader.upload(localFilePath, {
+                resource_type: "auto"
+            })
+            //file has been uploaded 
+            console.log("file has been uploaded on Cloudinary", response.url);
+            return response
+        } catch (error) {
+            fs.unlinkSync(localFilePath) // remove the locally save temp file as the upload opration got failed
+            return null;
+            
+        }
+    }
+
+
+    export {uploadOnCloud}
+    // Upload an image
+    //  const  uploadResult = await cloudinary.uploader
+    //    .upload(
+    //        'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+    //            public_id: 'shoes',
+    //        }
+    //    )
+    //    .catch((error) => {
+    //        console.log(error);
+    //    });
+    
+    // console.log(uploadResult);
+    
