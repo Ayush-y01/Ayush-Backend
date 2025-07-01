5fd9a02d27274079ceaf7ccd36676b4e8efd6754
Ayush-y01 <Ayush-y01>
2025-07-01 11:21:52 +0530

    add subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -30,8 +30,6 @@ const generateAccessAndRefreshToken = async (userId) => {
 
 
 
-
-
 const registerUser = asyncHandler( async (req, res, ) => {
     
     const {fullname, email, username, password} = req.body
